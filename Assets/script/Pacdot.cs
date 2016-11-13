afe159ae840e927bed6190f666271cf1da6ce273
Peter Wang <beijingwang@gmail.com>
2016-11-13 03:33:34 -0500

    init

---
+++ Assets/script/Pacdot.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pacdot : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //Check if the isTrigger option on th Collider2D is set to true or false
+        if (this.GetComponent<Collider2D>().isTrigger)
+        {
+            Debug.Log("This Collider2D can be triggered");
+        }
+        else
+        {
+            Debug.Log("This Collider2D cannot be triggered");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+	}
+
+    void onTriggerEnter2D(Collider2D co)
+    {
+        Debug.Log(co.name);
+        if (co.name == "pacman")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
